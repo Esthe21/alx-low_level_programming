670f01888d97a99aa4704d7444ffca2c3f797ab2
Esthe21 <sthrdogbey@gmail.com>
2023-04-21 21:08:59 -0700

    alx

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,18 @@
  */
 int main(void)
 {
-	char letter;
+	int i = 0;
+	char c = 'a';
 
-	letter = 'z';
-	while (letter >= 'a')
+	while (i < 10)
 	{
-		putchar(letter);
-		--letter;
+		putchar(i + '0');
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
