4fd4b62ae52ca4d672a0b53d4447bb3951bfb6de
Esthe21 <sthrdogbey@gmail.com>
2023-04-21 20:36:50 -0700

    missing q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: without q an
+ *Return: always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			++letter;
+			continue;
+		}
+		else
+		{
+			putchar(letter);
+			++letter;
+		}
+	}
+	return (0);
+}
