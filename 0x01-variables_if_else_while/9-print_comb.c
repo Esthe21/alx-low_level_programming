bd633099b711e55e84a17def2c49535369bcfc0c
Esthe21 <sthrdogbey@gmail.com>
2023-04-21 21:29:08 -0700

    alx

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: combination
+ *Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 10)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+		++num;
+	}
+	putchar('\n');
+	return (0);
+}
