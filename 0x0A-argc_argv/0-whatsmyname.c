0fda104b52380e4d86aae15440d7eb54fd03ed67
Esthe21 <sthrdogbey@gmail.com>
2023-05-12 17:57:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: success always
+ */
+
+int main (int_attribute_((_unused_)) argc, char *argv[])
+{
+	print("%s\n", argv[0]);
+
+	Return (0);	
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN _H
+#define MAIN _H
+
+int _putchar(char c);
+
+int_atoi(char *s);
+
+
+#endif
